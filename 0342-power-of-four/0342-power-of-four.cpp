8daa91332cf2210883cba9d4a070e52eaa590497
Prabhunandan Kumar <163457276+Milotn2023@users.noreply.github.com>
2025-08-15 08:02:10 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.41%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -4,6 +4,6 @@ public:
         if(n<=0)return false;
         int srt=sqrt(n);
         if(srt*srt!=n)return false;
-        return ((int)pow(2,15)%(int)srt==0);
+        return !((int)pow(2,15)%srt);
     }
 };

a786f6262519cb55899442da92487a7134d021a1
Prabhunandan Kumar <163457276+Milotn2023@users.noreply.github.com>
2025-08-15 13:09:09 +0530

    Time: 0 ms (100%), Space: 9.9 MB (10.84%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>ans(numRows);
-        ans[0].push_back(1);
-        if(numRows==1)return ans;
-        // ans[1]={1,1};
-        for(int i=1;i<numRows;i++){
-            vector<int>an=ans[i-1];
-            vector<int>demo(an.size()+1);
-            for(int j=0;j<an.size();j++){
-                if(j==0){
-                    demo[j]=1;
-                }
-                else
-                demo[j]=(an[j]+an[j-1]);
-            }
-            demo[an.size()]=1;
-            ans[i]=demo;
+        // vector<vector<int>>ans(numRows);
+        // ans[0].push_back(1);
+        // if(numRows==1)return ans;
+        // // ans[1]={1,1};
+        // for(int i=1;i<numRows;i++){
+        //     vector<int>an=ans[i-1];
+        //     vector<int>demo(an.size()+1);
+        //     for(int j=0;j<an.size();j++){
+        //         if(j==0){
+        //             demo[j]=1;
+        //         }
+        //         else
+        //         demo[j]=(an[j]+an[j-1]);
+        //     }
+        //     demo[an.size()]=1;
+        //     ans[i]=demo;
+        // }
+        // return ans;
+        vector<vector<int>> ans;
+    ans.reserve(numRows); // avoid reallocations
+
+    for (int i = 0; i < numRows; i++) {
+        ans.push_back(vector<int>(i + 1, 1)); // first and last = 1
+        for (int j = 1; j < i; j++) {
+            ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
         }
-        return ans;
+    }
+    return ans;
     }
 };

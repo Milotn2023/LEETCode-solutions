4e90800e47cf51fd57c21b0dcb75c6efae6dd9e3
Prabhunandan Kumar <163457276+Milotn2023@users.noreply.github.com>
2025-08-19 12:42:37 +0530

    Time: 1 ms (50.61%), Space: 112.5 MB (5.04%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -4,20 +4,15 @@ public:
     int n=nums.size();
     vector<int>v;
     int count=0;
+    long long ans=0;
     for(int i=0;i<n;i++){
-        if(nums[i]==0)count++;
+        if(nums[i]==0){count++;ans+=count;}
         else{
             if(count!=0){
                 v.push_back(count);
                 count=0;
             }
         }
-    }
-    if(count!=0)v.push_back(count);
-    long long ans=0;
-    for(int i=0;i<v.size();i++){
-        int t=v[i];
-        ans+=(1ll*t*(t+1))/2;
     }
     return ans;
     }

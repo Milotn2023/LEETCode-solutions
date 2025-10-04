048e9cb57c0145144080fb5a22b094948a76d6ba
Prabhunandan Kumar <163457276+Milotn2023@users.noreply.github.com>
2025-10-04 18:42:10 +0530

    Time: 206 ms (98.03%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select *
 from Cinema
-where id%2=1 and description <> 'boring'
+where id%2=1 and description != 'boring'  -- MOD(id.2)=1
 order by rating desc
